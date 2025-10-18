b5dd00af08d3eaa44f11234f88c1418d8cd8c565
leandrofa1980 <leandrofa1980@gmail.com>
2025-10-18 10:43:07 -0300

    Autorização com token, alteração da key no appsetings.json

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using minimal_api.Dominio.Entidades;
 using minimal_api.Dominio.Enuns;
 using minimal_api.Dominio.Interfaces;
@@ -16,7 +20,7 @@ using minimal_api.Infraestrutura.Db;
 var builder = WebApplication.CreateBuilder(args);
 
 var key = builder.Configuration.GetSection("Jwt").ToString();
-if (!string.IsNullOrEmpty(key)) key = "123456";
+if (!string.IsNullOrEmpty(key)) key = "Replace_with_a_secure_random_key_with_at_least_32_chars!";
 
 builder.Services.AddAuthentication(option =>
 {
@@ -26,6 +30,8 @@ builder.Services.AddAuthentication(option =>
 {
   option.TokenValidationParameters = new TokenValidationParameters
   {
+    ValidateIssuer = false,
+    ValidateAudience = false,
     ValidateLifetime = true,
     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
   };
@@ -37,7 +43,33 @@ builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
 builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+  options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+  {
+    Name = "Authorization",
+    Type = SecuritySchemeType.Http,
+    Scheme = "bearer",
+    BearerFormat = "JWT",
+    In = ParameterLocation.Header,
+    Description = "Insira o token JWT aqui"
+  });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+  {
+      {
+          new OpenApiSecurityScheme
+          {
+              Reference = new OpenApiReference
+              {
+                  Type = ReferenceType.SecurityScheme,
+                  Id = "Bearer"
+              }
+          },
+          new string[] {}
+      }
+  });
+});
 
 builder.Services.AddDbContext<DbContexto>(options =>
 {
@@ -51,17 +83,50 @@ var app = builder.Build();
 #endregion
 
 #region Home
-app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
+app.MapGet("/", () => Results.Json(new Home())).AllowAnonymous().WithTags("Home");
 #endregion
 
 #region Administradores
+
+string GerarTokenLwt(Administrador administrador)
+{
+  if (string.IsNullOrEmpty(key)) return string.Empty;
+
+  var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+  var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+  var claims = new List<Claim>()
+  {
+    new Claim("Email", administrador.Email),
+    new Claim("Perfil", administrador.Perfil),
+
+  };
+
+  var token = new JwtSecurityToken(
+    claims: claims,
+    expires: DateTime.Now.AddDays(1),
+    signingCredentials: credentials
+  );
+  return new JwtSecurityTokenHandler().WriteToken(token);
+}
+
 app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
-  if (administradorServico.Login(loginDTO) != null)
-    return Results.Ok("Login realizado com sucesso!");
+  var adm = administradorServico.Login(loginDTO);
+  if (adm != null)
+  {
+    string token = GerarTokenLwt(adm);
+    return Results.Ok(new AdministradorLogado
+    {
+      Email = adm.Email,
+      Perfil = adm.Perfil,
+      Token = token
+
+    });
+  }
   else
     return Results.Unauthorized();
-}).WithTags("Administradores");
+}).AllowAnonymous().WithTags("Administradores");
 
 app.MapGet("/administradores", ([FromQuery] int? pagina, IAdministradorServico administradorServico) =>
 {
@@ -94,34 +159,34 @@ app.MapGet("/administradores/{id}", ([FromRoute] int id, IAdministradorServico a
 
 app.MapPost("/administradores", ([FromBody] AdministradorDTO administradorDTO, IAdministradorServico administradorServico) =>
 {
-var validacao = new ErrosDeValidacao
-{
-  Mensagens = new List<string>()
-};
+  var validacao = new ErrosDeValidacao
+  {
+    Mensagens = new List<string>()
+  };
 
-if (string.IsNullOrEmpty(administradorDTO.Email))
-  validacao.Mensagens.Add("Email não pode ser vazio");
-if (string.IsNullOrEmpty(administradorDTO.Senha))
-  validacao.Mensagens.Add("Senha não pode ser vazia");
-if (administradorDTO.Perfil == null)
-  validacao.Mensagens.Add("Perfil não pode ser vazio");
+  if (string.IsNullOrEmpty(administradorDTO.Email))
+    validacao.Mensagens.Add("Email não pode ser vazio");
+  if (string.IsNullOrEmpty(administradorDTO.Senha))
+    validacao.Mensagens.Add("Senha não pode ser vazia");
+  if (administradorDTO.Perfil == null)
+    validacao.Mensagens.Add("Perfil não pode ser vazio");
 
-if (validacao.Mensagens.Count > 0)
-  return Results.BadRequest(validacao);
+  if (validacao.Mensagens.Count > 0)
+    return Results.BadRequest(validacao);
 
-var administrador = new Administrador
-{
-  Email = administradorDTO.Email,
-  Senha = administradorDTO.Senha,
-  Perfil = administradorDTO.Perfil.ToString() ?? Perfil.Editor.ToString()
-};
-administradorServico.Incluir(administrador);
-return Results.Created("$/administrador/{administrador.Id}", new AdministradorModelView
-{
-  Id = administrador.Id,
-  Email = administrador.Email,
-  Perfil = administrador.Perfil
-});
+  var administrador = new Administrador
+  {
+    Email = administradorDTO.Email,
+    Senha = administradorDTO.Senha,
+    Perfil = administradorDTO.Perfil.ToString() ?? Perfil.Editor.ToString()
+  };
+  administradorServico.Incluir(administrador);
+  return Results.Created("$/administrador/{administrador.Id}", new AdministradorModelView
+  {
+    Id = administrador.Id,
+    Email = administrador.Email,
+    Perfil = administrador.Perfil
+  });
 
 }).RequireAuthorization().WithTags("Administradores");
 #endregion
